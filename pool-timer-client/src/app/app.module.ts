7edfe0bead057ddae4b4c54072862a6977158332
Anuradha4dm <damithanuradha44@gmail.com>
2024-07-29 22:58:54 +0530

    add initial client interface

---
+++ pool-timer-client/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule, CommonModule],
+    exports: [AppComponent],
+    bootstrap:[AppComponent]
+})
+export class AppModule {}
